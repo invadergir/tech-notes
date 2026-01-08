fea75ec0ecc763c3afcc1ddef44cb537a65750fb
M Sesterhenn <invadergir@users.noreply.github.com>
2026-01-08 16:49:07 -0600

    add vscode / codium different configs

--- vscode/config/update-these.sh
+++ vscode/config/update-these.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 set -ex
-DIR=$HOME/.config/VSCodium/User
-cp -vf "$DIR"/{keybindings.json,settings.json,tasks.json} .
+codiumDIR=$HOME/.config/VSCodium/User
+vscodeDIR=$HOME/.config/Code/User
+
+cp -vf "$codiumDIR"/{keybindings.json,settings.json,tasks.json} vscodium/
+cp -vf "$vscodeDIR"/{keybindings.json,settings.json,tasks.json} vscode/
 set +x
 echo Done.
